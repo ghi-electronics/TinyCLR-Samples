6164dc66571392c9629732f39d374a9a6ee559a6
John Brochue <johnbrochue@outlook.com>
2018-09-07 16:12:58 -0400

    Add sample for APA102C led strip

---
+++ APA102CLedStrip/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Spi;
+using GHIElectronics.TinyCLR.Drivers.ShijiLighting.APA102C;
+using GHIElectronics.TinyCLR.Pins;
+
+namespace APA102CLedStrip {
+    public static class Program {
+        public static void Main() {
+            var spi1Controller = SpiController.FromName(FEZ.SpiBus.Spi1);
+
+            var ledCount = 8 * 32;
+            var apa102c = new APA102CController(spi1Controller.GetDevice(APA102CController.GetConnectionSettings()), ledCount);
+            var i = 0;
+
+            while (true) {
+                apa102c.Set(i, Color.Blue);
+                apa102c.Flush();
+                Thread.Sleep(125);
+
+                apa102c.Set(i, Color.Black);
+                apa102c.Flush();
+                Thread.Sleep(125);
+
+                if (++i > ledCount)
+                    i = 0;
+            }
+        }
+    }
+}
