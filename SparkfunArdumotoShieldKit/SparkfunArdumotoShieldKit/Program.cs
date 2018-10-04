59471ef15f665f22f5c47936768a400f346db8e7
Experiment-626 <aron.phillips@ghielectronics.com>
2018-10-04 14:29:22 -0400

    Initial base program.

---
+++ SparkfunArdumotoShieldKit/SparkfunArdumotoShieldKit/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+
+namespace SparkfunArdumotoShieldKit {
+    class Program {
+        static void Main() {
+        }
+    }
+}
