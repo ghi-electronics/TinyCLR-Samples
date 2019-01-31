8f06d5e12e7c267376b479864412aa2c90918d63
Gus Issa <gus.issa@ghielectronics.com>
2019-01-31 15:06:36 -0500

    Blinky example

---
+++ G80/Blinky/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Pins;
+
+namespace Blinky {
+    class Program {
+        static void Main() {
+            // Blink LED1
+            var LED = GpioController.GetDefault().OpenPin(G80.GpioPin.PE14);
+            LED.SetDriveMode(GpioPinDriveMode.Output);
+            var state = false;
+            while (true) {
+                LED.Write(state ? GpioPinValue.High : GpioPinValue.Low);
+                state = !state;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
