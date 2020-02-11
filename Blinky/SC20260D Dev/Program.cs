9039ce661f69bf2e39fe28cbdc5df1985c58af7a
Greg Norris <greg.norris@ghielectronics.com>
2020-02-11 14:50:01 -0500

    Restructure Samples and Add Blinky for SC20100 and SC20260D Dev boards

---
+++ Blinky/SC20260D Dev/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Pins;
+
+namespace Blinky{
+    class Program{
+        static void Main(){
+            // Blink SC20260D LED PH6
+            var led = GpioController.GetDefault().OpenPin(SC20260.GpioPin.PH6);
+            led.SetDriveMode(GpioPinDriveMode.Output);
+            var state = false;
+
+            while (true){
+                led.Write(state ? GpioPinValue.High : GpioPinValue.Low);
+                state = !state;
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
