6254fff7a65fd84066308d66dfee4db90c080b4d
Greg Norris <greg.norris@ghielectronics.com>
2020-03-04 14:23:18 -0500

    Added Graphing to Mic Click Sample

--- Accessories/Mikro Clicks/Mic Click/Program.cs
+++ Accessories/Mikro Clicks/Mic Click/Program.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Diagnostics;
-using GHIElectronics.TinyCLR.Pins;
-using Mikro.Click;
+using System.Drawing;
 using System.Threading;
+using GHIElectronics.TinyCLR.Pins;
 using GHIElectronics.TinyCLR.Devices.Gpio;
+using GHIElectronics.TinyCLR.Devices.Spi;
+using GHIElectronics.TinyCLR.Drivers.Sitronix.ST7735;
+using GHIElectronics.TinyCLR.Devices.Display;
+using Mikro.Click;
 
 namespace Mic_Click {
     class Program {
+        private static int x;
         static void Main() {
 
             ////////// Set these to match your board //////////////
@@ -14,11 +20,73 @@ namespace Mic_Click {
 
             var sensor = new MicClick(adc);
 
+            //TestWithSimpleDebug(sensor);
+            TestWithGraph(sensor);
+
+            //never get here!
+            Thread.Sleep(Timeout.Infinite);
+
+        }
+        static void TestWithSimpleDebug(MicClick sensor) {
             while (true) {
                 Debug.WriteLine(sensor.Read().ToString());
                 Thread.Sleep(100);
+            }
+        }
+        static void TestWithGraph(MicClick sensor) {
 
+            const int SCREEN_WIDTH = 160;
+            const int SCREEN_HEIGHT = 128;
+            int graph;
+            
+            var spi = SpiController.FromName(SC20100.SpiBus.Spi3);
+            var gpio = GpioController.GetDefault();
+
+            //Display Setup
+            var st7735 = new ST7735Controller(
+                spi.GetDevice(ST7735Controller.GetConnectionSettings
+                (SpiChipSelectType.Gpio, SC20100.GpioPin.PD10)), //CS pin.
+                gpio.OpenPin(SC20100.GpioPin.PC4), //RS pin.
+                gpio.OpenPin(SC20100.GpioPin.PE15) //RESET pin.
+            );
+
+            var backlight = gpio.OpenPin(SC20100.GpioPin.PE5);
+            backlight.SetDriveMode(GpioPinDriveMode.Output);
+            backlight.Write(GpioPinValue.High);
+
+            var displayController = DisplayController.FromProvider(st7735);
+            st7735.SetDataAccessControl(true, true, false, false); //Rotate the screen.
+
+            displayController.SetConfiguration(new SpiDisplayControllerSettings {
+                Width = SCREEN_WIDTH,
+                Height = SCREEN_HEIGHT,
+                DataFormat = DisplayDataFormat.Rgb565
+            });
+
+            displayController.Enable();
+
+            // Create flush event
+            Graphics.OnFlushEvent += Graphics_OnFlushEvent;
+
+            var screen = Graphics.FromImage(new Bitmap
+                (displayController.ActiveConfiguration.Width,
+                displayController.ActiveConfiguration.Height));
+
+            while (true) {
+
+                graph = (int)(sensor.Read() * 100) + 20;
+
+                if (x++ > SCREEN_WIDTH) {
+                    x = 0;
+                    screen.Clear(Color.Black);
+                }
+                screen.DrawLine(new Pen(Color.Green), x, SCREEN_HEIGHT, x, graph);
+                screen.Flush();
+            }
+            void Graphics_OnFlushEvent(IntPtr hdc, byte[] data) {
+                st7735.DrawBuffer(data);
             }
         }
     }
 }
+
