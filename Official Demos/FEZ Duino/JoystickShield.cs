bab67c234c4818c822c34605689551ab77737b87
Gus Issa <gus.issa@ghielectronics.com>
2020-04-11 11:17:50 -0400

    Add joystick shield

---
+++ Official Demos/FEZ Duino/JoystickShield.cs
@@ -0,0 +1,5 @@
+// commonly available from many sources
+// Buttons are connected D2 to D7 in this order A to F
+// Joystick is on A0 and A1 with button on D8
+// remember to set the switch to 3v
+// Simply use GPIO and ADC, no drivers are necessary
