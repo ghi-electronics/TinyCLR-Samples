4c0ada255cd6d265f3ca5cca92cca4fe4be55056
Gus Issa <gus.issa@ghielectronics.com>
2020-04-11 11:27:24 -0400

    Add EL Escudo Dos shield

---
+++ Official Demos/FEZ Duino/ElEscudoDosShield.cs
@@ -0,0 +1,4 @@
+// https://www.sparkfun.com/products/10878
+// simple optically-isolated control of high voltage to control EL lights
+// Sparkfun offer those EL light as well
+// use GPIO to control, no drivers are necessary
