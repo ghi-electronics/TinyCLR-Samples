8164640a5aff886a441db093539041eedae1c26e
Gus Issa <gus.issa@ghielectronics.com>
2020-04-13 17:53:40 -0400

    add micro rover

---
+++ Official Demos/FEZ Bit/MicroRover.cs
@@ -0,0 +1,26 @@
+// Made by http://freenove.com/
+// https://github.com/Freenove/Makecode-Extension-Rover/blob/master/rover.ts
+// P0 buzzer
+// P1 light sensor, analog
+// P2 battery level, analog
+// P12 ultrasonic trig
+// P13 ultrasonic echo
+// P14 line sensor, digital
+// P15 line sensor, digital
+// P15 line sensor, digital
+// P19 I2C for PWM PCA9685... address 0x43
+// P20 I2C for PWM PCA9685
+
+
+// ON PWM Chip channels
+// motor1 0,1
+// motor2 2,3
+// RGB1 14,15,13
+// RGB2 5,6,4
+// RGB3 8,9,7
+// RGB4 11,12,10
+
+// Ch14 Ch15 right motor
+
+
+
