7253c242978a2227108dd55c9d134ece0af11cad
Mif Masterz <mifmasterz@yahoo.com>
2020-06-17 15:28:18 +0700

    minor changes - space in welcome message add delay after send success code and before set new baud rate

--- Applications/FileSystem/uAlfat.Core/uAlfatModule.cs
+++ Applications/FileSystem/uAlfat.Core/uAlfatModule.cs
@@ -62,8 +62,8 @@ namespace uAlfat.Core
         void PrintStartUpMessage() {
             var appVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
             var bootVer = Resources.GetString(Resources.StringResources.BOOTLOADER_VER);
-            Bus.WriteLine($"GHI Electronics, LLC{Strings.NewLine} ----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} μALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
-            Console.WriteLine($"GHI Electronics, LLC{Strings.NewLine} ----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} μALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
+            Bus.WriteLine($"GHI Electronics, LLC{Strings.NewLine}----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} uALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
+            Console.WriteLine($"GHI Electronics, LLC{Strings.NewLine}----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} uALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
         }
 
         private void ProcessCommand(string data)
@@ -1032,6 +1032,7 @@ namespace uAlfat.Core
                         if (newBaudRate > 0)
                         {
                             Bus.WriteLine(ResponseCode.Success);
+                            Thread.Sleep(100);
                             Bus.SetBaudRate(newBaudRate);
                             Bus.WriteLine(ResponseCode.Success);
                         }
