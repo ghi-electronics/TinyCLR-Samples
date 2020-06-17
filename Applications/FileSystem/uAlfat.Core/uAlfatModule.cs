04bc23ed2449e4354a2f85136cef2d73af92789a
Mif Masterz <mifmasterz@yahoo.com>
2020-06-17 10:19:49 +0700

    print startup message

--- Applications/FileSystem/uAlfat.Core/uAlfatModule.cs
+++ Applications/FileSystem/uAlfat.Core/uAlfatModule.cs
@@ -55,14 +55,14 @@ namespace uAlfat.Core
             this.StorageControllerName = storageControllerName;
             this.SDControllerName = sDControllerName;
             this.InitUsbHost();
-            //Console.WriteLine("uAlfat is ready");
+            Console.WriteLine("uAlfat is ready");
             this.PrintStartUpMessage();
         }
 
         void PrintStartUpMessage() {
             var appVer = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
             var bootVer = Resources.GetString(Resources.StringResources.BOOTLOADER_VER);
-            //Bus.WriteLine($"GHI Electronics, LLC{Strings.NewLine} ----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} μALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
+            Bus.WriteLine($"GHI Electronics, LLC{Strings.NewLine} ----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} μALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
             Console.WriteLine($"GHI Electronics, LLC{Strings.NewLine} ----------------------------- {Strings.NewLine} Boot Loader {bootVer} {Strings.NewLine} μALFAT(TM) {appVer} {Strings.NewLine}{ResponseCode.Success}");
         }
 
