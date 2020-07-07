ea9f4c8bacedf6269e37aa01445c9701b1988643
Dat <dat.tran@ghielectronics.com>
2020-07-07 11:22:01 -0400

    Close rts/cts manually before change baudrate

--- Applications/FileSystem/uAlfat.Core/Helpers/CommunicationsBus.cs
+++ Applications/FileSystem/uAlfat.Core/Helpers/CommunicationsBus.cs
@@ -1,4 +1,8 @@
+using GHIElectronics.TinyCLR.Devices.Gpio.Provider;
 using GHIElectronics.TinyCLR.Devices.Uart;
+using GHIElectronics.TinyCLR.Devices.UsbHost.Descriptors;
+using GHIElectronics.TinyCLR.Native;
+using GHIElectronics.TinyCLR.Pins;
 using System;
 using System.Collections;
 using System.Diagnostics;
@@ -6,10 +10,8 @@ using System.IO;
 using System.Text;
 using System.Threading;
 
-namespace uAlfat.Core
-{
-    public class CommunicationsBus : IDisposable
-    {
+namespace uAlfat.Core {
+    public class CommunicationsBus : IDisposable {
         private string TempData { set; get; } = string.Empty;
         private CommunicationsProtocal protocal;
         private StreamReader commandFile;
@@ -17,8 +19,8 @@ namespace uAlfat.Core
         private bool disposed;
         public event IncomingDataEventHandler DataReceived;
         public delegate void IncomingDataEventHandler(string data);
-        public enum CommunicationsProtocal
-        {
+        private string uartPort;
+        public enum CommunicationsProtocal {
             Uart,
             Spi,
             I2C
@@ -30,16 +32,19 @@ namespace uAlfat.Core
                 DataBits = 8,
                 Parity = UartParity.None,
                 StopBits = UartStopBitCount.One,
-                Handshaking = UartHandshake.None
+                Handshaking = UartHandshake.RequestToSend
 
             };
 
+            CloseHandshakingPins(this.uartPort);
+
             this.serialPort.SetActiveSettings(uartSetting);
         }
-        public CommunicationsBus(string uartPort,int baudRate = 9600) {
+        public CommunicationsBus(string uartPort, int baudRate = 9600) {
             this.disposed = false;
             this.commandFile = null;
             this.protocal = CommunicationsProtocal.Uart;
+            this.uartPort = uartPort;
 
             this.serialPort = UartController.FromName(uartPort);
 
@@ -48,7 +53,7 @@ namespace uAlfat.Core
                 DataBits = 8,
                 Parity = UartParity.None,
                 StopBits = UartStopBitCount.One,
-                Handshaking = UartHandshake.None
+                Handshaking = UartHandshake.RequestToSend
 
             };
 
@@ -62,8 +67,7 @@ namespace uAlfat.Core
             this.serialPort.DataReceived += this.SerialPort_DataReceived;
 
         }
-        private void SerialPort_DataReceived(UartController sender, DataReceivedEventArgs e)
-        {
+        private void SerialPort_DataReceived(UartController sender, DataReceivedEventArgs e) {
             var rxBuffer = new byte[e.Count];
             var bytesReceived = this.serialPort.Read(rxBuffer, 0, e.Count);
             var dataStr = Encoding.UTF8.GetString(rxBuffer, 0, bytesReceived);
@@ -76,38 +80,32 @@ namespace uAlfat.Core
 
             //Debug.WriteLine(dataStr);
             this.TempData += dataStr;
-            if (dataStr.IndexOf(Strings.NewLine) > -1)
-            {
+            if (dataStr.IndexOf(Strings.NewLine) > -1) {
                 DataReceived?.Invoke(this.TempData.Trim());
                 this.TempData = string.Empty;
             }
-            
+
         }
         public CommunicationsBus(string portName, StreamReader commandFile) : this(portName) => this.commandFile = commandFile;
 
-        public void Dispose()
-        {
+        public void Dispose() {
             this.Dispose(true);
 
             GC.SuppressFinalize(this);
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
+        protected virtual void Dispose(bool disposing) {
             if (this.disposed)
                 return;
 
-            if (disposing)
-            {
-                if (this.commandFile != null)
-                {
+            if (disposing) {
+                if (this.commandFile != null) {
                     this.commandFile.Close();
                     this.commandFile.Dispose();
                     this.commandFile = null;
                 }
 
-                if (this.serialPort != null)
-                {
+                if (this.serialPort != null) {
                     this.serialPort.Disable();
                     this.serialPort.Dispose();
                     this.serialPort = null;
@@ -117,17 +115,14 @@ namespace uAlfat.Core
             this.disposed = true;
         }
 
-        ~CommunicationsBus()
-        {
+        ~CommunicationsBus() {
             this.Dispose(false);
         }
 
-        public void Write(string data)
-        {
+        public void Write(string data) {
             data = data.Trim();
 
-            switch (this.protocal)
-            {
+            switch (this.protocal) {
                 case CommunicationsProtocal.I2C:
                     throw new NotImplementedException();
 
@@ -161,12 +156,10 @@ namespace uAlfat.Core
             }
         }
 
-        public void WriteLine(string line)
-        {
+        public void WriteLine(string line) {
             line = line.Trim();
 
-            switch (this.protocal)
-            {
+            switch (this.protocal) {
                 case CommunicationsProtocal.I2C:
                     throw new NotImplementedException();
 
@@ -174,16 +167,15 @@ namespace uAlfat.Core
                     throw new NotImplementedException();
 
                 case CommunicationsProtocal.Uart:
-                    
-                    var databytes = UTF8Encoding.UTF8.GetBytes(line+ Strings.NewLine);
+
+                    var databytes = UTF8Encoding.UTF8.GetBytes(line + Strings.NewLine);
                     this.serialPort.Write(databytes);
 
                     break;
             }
         }
 
-        public bool ProcessCommandFromFile()
-        {
+        public bool ProcessCommandFromFile() {
             if (this.commandFile == null) throw new InvalidOperationException("This object was not initialized for a command file.");
 
             var line = this.commandFile.ReadLine(); //Possible make this non-blocking
@@ -195,25 +187,20 @@ namespace uAlfat.Core
             return true;
         }
 
-        public string ReadLine()
-        {
-            if (this.serialPort != null)
-            {
-                if (this.serialPort.BytesToRead > 0)
-                {
+        public string ReadLine() {
+            if (this.serialPort != null) {
+                if (this.serialPort.BytesToRead > 0) {
                     var rxBuffer = new byte[this.serialPort.BytesToRead];
                     var bytesReceived = this.serialPort.Read(rxBuffer, 0, this.serialPort.BytesToRead);
                     var dataStr = Encoding.UTF8.GetString(rxBuffer, 0, bytesReceived);
                     return dataStr;
                 }
-                 
+
             }
-            else if (this.commandFile != null)
-            {
+            else if (this.commandFile != null) {
                 return this.commandFile.ReadLine(); //Handle end of file
             }
-            else
-            {
+            else {
                 throw new NotImplementedException();
             }
             return string.Empty;
@@ -239,6 +226,42 @@ namespace uAlfat.Core
             }
             else {
                 throw new NotImplementedException();
+            }
+        }
+
+        private static void CloseHandshakingPins(string uartPort) {
+            var cts = -1;
+            var rts = -1;
+
+            // This is Workaround  to fix the issue: https://github.com/ghi-electronics/TinyCLR-Libraries/issues/641
+            switch (uartPort) {
+                case SC20260.UartPort.Usart2:
+                    cts = SC20260.GpioPin.PD3;
+                    rts = SC20260.GpioPin.PD4;
+                    break;
+
+                case SC20260.UartPort.Uart4:
+                    cts = SC20260.GpioPin.PB0;
+                    rts = SC20260.GpioPin.PA15;
+                    break;
+                case SC20260.UartPort.Uart5:
+                    cts = SC20260.GpioPin.PC9;
+                    rts = SC20260.GpioPin.PC8;
+                    break;
+
+                case SC20260.UartPort.Uart7:
+                    cts = DeviceInformation.DeviceName.CompareTo("SC20260") == 0 ? SC20260.GpioPin.PF9 : SC20260.GpioPin.PE10;
+                    rts = DeviceInformation.DeviceName.CompareTo("SC20260") == 0 ? SC20260.GpioPin.PF8 : SC20260.GpioPin.PE9;
+                    break;
+            }
+
+            if (cts >= 0 && rts >= 0) {
+                var gpioController = new GpioControllerApiWrapper(NativeApi.Find(NativeApi.GetDefaultName(NativeApiType.GpioController), NativeApiType.GpioController));
+
+                gpioController.ClosePin(cts);
+                gpioController.ClosePin(rts);
+
+                gpioController.Dispose();
             }
         }
     }
