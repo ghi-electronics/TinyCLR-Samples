edbea9e31b4be2e5646e17db3658cdf0a1af69b0
Dat <dat.tran@ghielectronics.com>
2020-07-14 09:18:01 -0400

    moved to accessories folder

--- Official Demos/FEZ Bit/KlefPiano.cs
+++
@@ -1,9 +0,0 @@
-// Klef piano by kitronics
-//https://github.com/KitronikLtd/pxt-kitronik-klef-piano/blob/master/ubit_piano.ts
-// P0 speaker
-
-// P19 I2C for PWM address 0x0D. AT42QT2160
-// P20 I2C for PWM
-
-// no other functionality!
-
--- Official Demos/FEZ Bit/RingBit.cs
+++
@@ -1,2 +0,0 @@
-// By Elecfreaks
-// simply P0, P1 and P2 with connectors to go to 2 servos and a digital line senor
